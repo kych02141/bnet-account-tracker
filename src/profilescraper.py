e8c60bb0c0bbe9408028d33d6dbbb4b3686c21b0
Nate Shoffner <nate.shoffner@gmail.com>
2020-05-24 15:00:40 -0400

    Separate scraping from entry code

---
+++ src/profilescraper.py
@@ -0,0 +1,57 @@
+import requests
+import urllib.parse
+from bs4 import BeautifulSoup
+from prestige import PRESTIGE_BORDERS, PRESTIGE_STARS
+
+def get_prestige_level(level, border_hash, star_hash):
+    prestige = 0
+
+    if border_hash and border_hash in PRESTIGE_BORDERS:
+        prestige += PRESTIGE_BORDERS.get(border_hash)
+
+    if star_hash and star_hash in PRESTIGE_STARS:
+        prestige += PRESTIGE_STARS.get(star_hash)
+
+    return level + (prestige * 100)
+
+
+def get_account_stats(account):
+
+    x = account.battletag.split('#')
+    name = urllib.parse.quote_plus(x[0])
+    id = x[1]
+
+    url = "https://playoverwatch.com/en-us/career/pc/%s-%s" % (name, id)
+
+    profile_response = requests.get(url, timeout=5)
+    
+    soup = BeautifulSoup(profile_response.content, "html.parser")
+
+    if not soup.findAll(text="Profile Not Found"):
+        level_div = soup.find("div", {"class": "player-level"})
+
+        level = int(level_div.text)
+        border_hash = level_div['style'].rpartition('/')[-1][:-6]
+        star_div = level_div.find('div', {"class": "player-rank"})
+
+        if star_div:
+            star_hash = star_div['style'].rpartition('/')[-1][:-6]
+        else:
+            star_hash = None
+
+        account.level = get_prestige_level(level, border_hash, star_hash)
+
+        tank_result = soup.find(
+            "div", {"data-ow-tooltip-text": "Tank Skill Rating"})
+        if tank_result:
+            account.tank_rating = tank_result.nextSibling.text
+
+        damage_result = soup.find(
+            "div", {"data-ow-tooltip-text": "Damage Skill Rating"})
+        if damage_result:
+            account.damage_rating = damage_result.nextSibling.text
+
+        support_result = soup.find(
+            "div", {"data-ow-tooltip-text": "Support Skill Rating"})
+        if support_result:
+            account.support_rating = support_result.nextSibling.text
--- src/run.py
+++ src/run.py
@@ -3,12 +3,9 @@
 
 import json
 import pyperclip
-import requests
 import time
-import urllib.parse
 from account import Account, BanStatus
-from bs4 import BeautifulSoup
-from prestige import PRESTIGE_BORDERS, PRESTIGE_STARS
+from profilescraper import *
 from tabulate import tabulate
 from threading import Thread
 
@@ -56,62 +53,6 @@ def get_accounts():
             account_index = account_index + 1
 
     return accounts
-
-
-def get_prestige_level(level, border_hash, star_hash):
-    prestige = 0
-
-    if border_hash and border_hash in PRESTIGE_BORDERS:
-        prestige += PRESTIGE_BORDERS.get(border_hash)
-
-    if star_hash and star_hash in PRESTIGE_STARS:
-        prestige += PRESTIGE_STARS.get(star_hash)
-
-    return level + (prestige * 100)
-
-
-def get_account_stats(account):
-
-    x = account.battletag.split('#')
-    name = urllib.parse.quote_plus(x[0])
-    id = x[1]
-
-    url = "https://playoverwatch.com/en-us/career/pc/%s-%s" % (name, id)
-
-    profile_response = requests.get(url, timeout=5)
-    
-    soup = BeautifulSoup(profile_response.content, "html.parser")
-
-    if not soup.findAll(text="Profile Not Found"):
-        level_div = soup.find("div", {"class": "player-level"})
-
-        level = int(level_div.text)
-        border_hash = level_div['style'].rpartition('/')[-1][:-6]
-        star_div = level_div.find('div', {"class": "player-rank"})
-
-        if star_div:
-            star_hash = star_div['style'].rpartition('/')[-1][:-6]
-        else:
-            star_hash = None
-
-        account.level = get_prestige_level(level, border_hash, star_hash)
-
-        tank_result = soup.find(
-            "div", {"data-ow-tooltip-text": "Tank Skill Rating"})
-        if tank_result:
-            account.tank_rating = tank_result.nextSibling.text
-
-        damage_result = soup.find(
-            "div", {"data-ow-tooltip-text": "Damage Skill Rating"})
-        if damage_result:
-            account.damage_rating = damage_result.nextSibling.text
-
-        support_result = soup.find(
-            "div", {"data-ow-tooltip-text": "Support Skill Rating"})
-        if support_result:
-            account.support_rating = support_result.nextSibling.text
-
-
 
 def get_avg_sr(accounts, role):
     placed_accts = 0
